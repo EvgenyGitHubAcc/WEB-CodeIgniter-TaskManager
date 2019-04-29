04f8adee0ac81fdadb3278f8fb3abaa7498ef552
bondarenea@mail.ru <bondarenEA@mail.ru>
2019-04-29 18:00:02 +0300

    Second commit with DBModel

--- config/config.php
+++ config/config.php
@@ -521,3 +521,25 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
+//$db['default'] = array(
+//    'dsn' => '',
+//    'hostname' => 'localhost',
+//    'username' => 'root', // Username
+//    'password' => '', // Password
+//    'database' => 'taskmanager', // Database name
+//    'dbdriver' => 'mysqli',
+//    'dbprefix' => '',
+//    'pconnect' => FALSE,
+//    'db_debug' => (ENVIRONMENT !== 'production'),
+//    'cache_on' => FALSE,
+//    'cachedir' => '',
+//    'char_set' => 'utf8',
+//    'dbcollat' => 'utf8_general_ci',
+//    'swap_pre' => '',
+//    'encrypt' => FALSE,
+//    'compress' => FALSE,
+//    'stricton' => FALSE,
+//    'failover' => array(),
+//    'save_queries' => TRUE
+//);
